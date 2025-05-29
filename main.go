c91367923da20a22db1377d295adaa2b910f7d53
OMGasm <OMGasm@users.noreply.github.com>
2025-05-29 16:14:43 +1000

    add a little file reading abstraction; change most fs.* calls to os.* calls for less fs<->path mess

--- main.go
+++ main.go
@@ -44,8 +44,9 @@ func main() {
 			perr("Unable to read config for some reason\n", err)
 		}
 	}
-	hw_dir := os.DirFS("/sys/class/hwmon")
-	dirs, err := fs.ReadDir(hw_dir, ".")
+
+	hw_dir := "/sys/class/hwmon"
+	dirs, err := os.ReadDir(hw_dir)
 	if err != nil {
 		perr("can't read hwmon?", err)
 		return
@@ -55,12 +56,9 @@ func main() {
 		if info.Mode()&fs.ModeSymlink == 0 {
 			continue
 		}
-		dev_fs, err := fs.Sub(hw_dir, dirmon.Name())
-		if err != nil {
-			perr("", err)
-		}
+		dev_path := filepath.Join(hw_dir, dirmon.Name())
 		println(dirmon.Name())
-		name_file, err := dev_fs.Open("name")
+		name_file, err := os.Open(filepath.Join(dev_path, "name"))
 		if err != nil {
 			perr("can't read device name?", err)
 		}
@@ -74,59 +72,68 @@ func main() {
 		name = strings.TrimSpace(name)
 
 		if name == cfg.Cpu_sensor {
+			dev_fs := os.DirFS(dev_path)
 			labels, err := fs.Glob(dev_fs, "temp*_label")
 			if err != nil {
 				perr("i don't evne know", err)
 			}
 			for _, label := range labels {
 				fmt.Println(name, label)
-				label_file, err := dev_fs.Open(label)
+				label_s, err := read_hwmon_file(filepath.Join(dev_path, "label"))
 				if err != nil {
-					perr("label file bad?", err)
+					perr("Failed to read probe label", err)
 					continue
 				}
-				var label_bytes [32]byte
-				size, err = label_file.Read(label_bytes[:])
-				if err != nil {
-					perr("err reading temp?", err)
-					continue
-				}
-				label_file.Close()
-				label_s := string(label_bytes[:size])
-				label_s = strings.TrimSpace(label_s)
 				fmt.Println(label_s)
 
 				tinput := strings.TrimSuffix(label, "_label")
 
-				path := filepath.Join("/sys/class/hwmon", dirmon.Name(), tinput+"_input")
+				path := filepath.Join(dev_path, tinput+"_input")
 				fmt.Println(tinput)
 				fmt.Println(path)
 
-				err = os.Remove("temp_input")
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
-					perr("remove temp symlink", err)
-				}
-				err = os.Symlink(path, "temp_input")
+				err = create_hwmon_symlink(path, "temp_input")
 				if err != nil {
-					perr("symlink", err)
-					continue
+					perr("Failed to create probe symlink", err)
+					return
 				}
-				tempf, err := os.Open("temp_input")
+
+				temp, err := read_hwmon_file("temp_input")
 				if err != nil {
-					perr("temp open", err)
-					continue
+					perr("Failed to read probe symlink", err)
+					return
 				}
-				var temp_bytes [32]byte
-				size, err := tempf.Read(temp_bytes[:])
-				if err != nil {
-					perr("temp read", err)
-					break
-				}
-				temp := string(temp_bytes[:size])
-				temp = strings.TrimSpace(temp)
 				fmt.Println(temp)
 			}
 		}
 		fmt.Println()
 	}
+}
+
+func create_hwmon_symlink(target_path string, link_path string) error {
+	err := os.Remove(link_path)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		perr("Failed to remove probe symlink", err)
+		return err
+	}
+	err = os.Symlink(target_path, link_path)
+	return err
+}
+
+func read_hwmon_file(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", nil
+	}
+
+	var file_bytes [32]byte
+	size, err := file.Read(file_bytes[:])
+	if err != nil {
+		return "", err
+	}
+
+	str := string(file_bytes[:size])
+	str = strings.TrimSpace(str)
+
+	return str, nil
 }
